d16193c8185dc5a872f154b70efb444c65f8377c
rsmoreira <rsmoreira.mail@gmail.com>
2017-10-17 22:52:05 -0200

    Lecture 57 - Configured API Weather Map into our application and added AXIOS dependency. AXIOS will help with ajax requests from the browser, it works almost identically as jQuery. Added Axios to handle our ajax requests from the browser.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const WEATHERMAP_API_KEY= '18d44d41a7e22a47ceb4ee9a2b8f87a9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHERMAP_API_KEY}`
+
+export const FECTH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const URL = `${ROOT_URL}&q=${city},br`;
+    const request = axios.get(URL);
+
+    return {
+        type: FECTH_WEATHER,
+        payload: request
+    };
+}
