9fb55a5a3dadaf1a4b505d3bf337216632f11001
rsmoreira <rsmoreira.mail@gmail.com>
2017-10-17 23:51:20 -0200

    Lecture 59 - Updated search_bar to bind it to the featchWeather, into our ActionCreator js file (/actions/index.js). In this case, our action will be called from the onFormSubmit function, on search_bar. Updated search_bar to be a real container, binding feachWeather action (through bindActionCreators from redux).

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './weather_reducer';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
